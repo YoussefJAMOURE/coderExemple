8ce204ac7321368e01035d32aa9b021c95fdf282
YoussefJAMOURE <y.jamoure@gmail.com>
2018-08-29 14:16:20 +0100

    app is ok

---
+++ src/main/java/com/Coder/coderApp/CoderAppApplication.java
@@ -0,0 +1,12 @@
+package com.Coder.coderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderAppApplication.class, args);
+	}
+}
